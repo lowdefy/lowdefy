4329645c8e5400d82c5484fd21cb4bf22dd8a495
Sam Tolmay <samtolmay@gmail.com>
2020-10-05 17:41:11 +0200

    chore: add file license headers

--- packages/serializer/src/stableStringify.js
+++ packages/serializer/src/stableStringify.js
@@ -3,7 +3,21 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-param-reassign */
 
-import type from '@lowdefy/type';
+/*
+   Copyright 2020 Lowdefy, Inc
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 // https://github.com/substack/json-stable-stringify
 // https://github.com/substack/json-stable-stringify/LICENCE
@@ -26,6 +40,8 @@ import type from '@lowdefy/type';
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import type from '@lowdefy/type';
 
 function stableStringify(obj, opts) {
   if (!opts) opts = {};
