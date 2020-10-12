68913acb36d94df2e3bc57c3d73a4d5ec50c984a
Sam Tolmay <samtolmay@gmail.com>
2020-10-12 13:21:39 +0200

    chore(tests): configure jest to collect coverage from all files in src dir

--- packages/nunjucks/jest.config.js
+++ packages/nunjucks/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
+  collectCoverageFrom: ['src/**/*.js'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
