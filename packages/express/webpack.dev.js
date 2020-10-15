c1a95d112f84864707d3b58981ddfefdd26ae070
Sam Tolmay <samtolmay@gmail.com>
2020-10-15 16:01:01 +0200

    chore: update webpack server config

--- packages/express/webpack.dev.js
+++ packages/express/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 const path = require('path');
 
@@ -7,6 +8,11 @@ module.exports = merge(common, {
   devtool: 'eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    port: 3001,
+    port: 3000,
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
 });
