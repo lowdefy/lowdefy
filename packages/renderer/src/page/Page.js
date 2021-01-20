b1547792a9e91560196da474d81effc61c1903d6
SamTolmay <samtolmay@gmail.com>
2021-01-20 14:11:34 +0200

    chore: Update licence header copywrite year.

--- packages/renderer/src/page/Page.js
+++ packages/renderer/src/page/Page.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 
 /*
-  Copyright 2020 Lowdefy, Inc
+  Copyright 2020-2021 Lowdefy, Inc
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
