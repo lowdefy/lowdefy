daced49ee28fa7c0863859875a3eb3fae4ef3b22
SamTolmay <samtolmay@gmail.com>
2021-04-09 12:58:30 +0200

    fix(graphql): Improve KnexBuilder errors, add KnexBuilder tests.

--- packages/graphql/src/connections/Knex/KnexBuilder/KnexBuilder.js
+++ packages/graphql/src/connections/Knex/KnexBuilder/KnexBuilder.js
@@ -25,12 +25,23 @@ async function knexBuilder({ request, connection }) {
   }
   for (const method of request.query) {
     if (Object.keys(method).length !== 1) {
-      throw new Error('Invalid query');
+      throw new Error(
+        `Invalid query, more than one method defined in a method object, received ${JSON.stringify(
+          Object.keys(method)
+        )}.`
+      );
     }
     const methodName = Object.keys(method)[0];
     const methodArgs = method[methodName];
     if (!type.isArray(methodArgs)) {
-      throw new Error('Invalid query');
+      throw new Error(
+        `Invalid query, method "${methodName}" arguments should be an array, received ${JSON.stringify(
+          methodArgs
+        )}.`
+      );
+    }
+    if (!type.isFunction(client[methodName])) {
+      throw new Error(`Invalid query builder method "${methodName}".`);
     }
     client = client[methodName](...methodArgs);
   }
