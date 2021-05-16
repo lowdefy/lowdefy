a6e2fe036160d49b6c1994aee68326f88fc7c564
Gervwyk <gervwyk@gmail.com>
2021-05-16 20:18:19 +0200

    fix(docs): Import _js.filterDefaultValue as operator.

--- packages/docs/public/modules/filterDefaultValue.js
+++ packages/docs/public/modules/filterDefaultValue.js
@@ -45,4 +45,4 @@ function filterDefaultValue(value, defaultValue) {
   return filterObject({ obj: value, path: [] });
 }
 
-module.exports = filterDefaultValue;
+window.lowdefy.registerJsOperator('filterDefaultValue', filterDefaultValue);
