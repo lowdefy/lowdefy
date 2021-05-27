12cb7829460e05479fc7376f49b0defa0819afea
SamTolmay <samtolmay@gmail.com>
2021-05-27 10:40:15 +0200

    fix(servers): Append html when serving index from url root

--- packages/cli/src/commands/dev/getExpress.js
+++ packages/cli/src/commands/dev/getExpress.js
@@ -21,27 +21,52 @@ import { readFile } from '@lowdefy/node-utils';
 import findOpenPort from '../../utils/findOpenPort';
 
 async function getExpress({ context, gqlServer, options }) {
+  let indexHtml = null;
+
+  const serveIndex = async (req, res) => {
+    if (!indexHtml) {
+      indexHtml = await readFile(path.resolve(__dirname, 'shell/index.html'));
+      let appConfig = await readFile(path.resolve(context.outputDirectory, 'app.json'));
+      appConfig = JSON.parse(appConfig);
+      indexHtml = indexHtml.replace(
+        '<!-- __LOWDEFY_APP_HEAD_HTML__ -->',
+        appConfig.html.appendHead
+      );
+      indexHtml = indexHtml.replace(
+        '<!-- __LOWDEFY_APP_BODY_HTML__ -->',
+        appConfig.html.appendBody
+      );
+    }
+    res.send(indexHtml);
+  };
+
   const app = express();
+
   app.set('port', parseInt(options.port));
+
   gqlServer.applyMiddleware({ app, path: '/api/graphql' });
 
   const reloadPort = await findOpenPort();
   const reloadReturned = await reload(app, { route: '/api/dev/reload.js', port: reloadPort });
 
+  // serve index.html with appended html
+  // else static server serves without appended html
+  app.get('/', serveIndex);
+
+  // serve public files
   app.use('/public', express.static(path.resolve(process.cwd(), 'public')));
+
+  // serve webpack files
   app.use(express.static(path.resolve(__dirname, 'shell')));
 
+  // serve version for renderer module federation
   app.use('/api/dev/version', (req, res) => {
     res.json(context.lowdefyVersion);
   });
-  app.use(async (req, res) => {
-    let indexHtml = await readFile(path.resolve(__dirname, 'shell/index.html'));
-    let appConfig = await readFile(path.resolve(context.outputDirectory, 'app.json'));
-    appConfig = JSON.parse(appConfig);
-    indexHtml = indexHtml.replace('<!-- __LOWDEFY_APP_HEAD_HTML__ -->', appConfig.html.appendHead);
-    indexHtml = indexHtml.replace('<!-- __LOWDEFY_APP_BODY_HTML__ -->', appConfig.html.appendBody);
-    res.send(indexHtml);
-  });
+
+  // Redirect all 404 to index.html with status 200
+  // This should always be the last route
+  app.use(serveIndex);
   return { expressApp: app, reloadFn: reloadReturned.reload };
 }
 
