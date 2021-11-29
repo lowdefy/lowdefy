4e23fec8a4985d7453dfcf750298bc0bedeb34a2
JohannMoller <mollerjohann23@gmail.com>
2021-11-29 11:09:00 +0200

    feat: Add _diff to client operators as well as server.

--- packages/plugins/operators/operators-diff/src/operators/types.js
+++ packages/plugins/operators/operators-diff/src/operators/types.js
@@ -13,10 +13,12 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
+import * as client from './operatorsClient.js';
 import * as server from './operatorsServer.js';
 
 export default {
   operators: {
+    client: Object.keys(client),
     server: Object.keys(server),
   },
 };
