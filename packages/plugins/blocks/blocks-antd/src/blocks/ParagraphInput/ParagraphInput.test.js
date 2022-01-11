15ec80f8417211d839e648b10efa25f98589547a
Gervwyk <gervwyk@gmail.com>
2022-01-11 09:58:04 +0200

    chore(blocks-antd): Update ParagraphInput tests.

--- packages/plugins/blocks/blocks-antd/src/blocks/ParagraphInput/ParagraphInput.test.js
+++ packages/plugins/blocks/blocks-antd/src/blocks/ParagraphInput/ParagraphInput.test.js
@@ -16,21 +16,18 @@
 
 import { runBlockSchemaTests, runRenderTests } from '@lowdefy/block-dev';
 
-import block from './index.js';
 import Block from './ParagraphInput.js';
 import examples from './examples.yaml';
 import schema from './schema.json';
 import validationsExamples from '../../validationExamples.js';
 
-// test: {
-//   validation: true,
-//   required: true,
-//   values: [
-//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-//   ],
-// }
+const testConfig = {
+  required: true,
+  validation: true,
+  values: [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  ],
+};
 
-const { meta, tests } = block;
-
-runRenderTests({ examples, Block, meta, schema, tests, validationsExamples });
-runBlockSchemaTests({ examples, meta, schema });
+runRenderTests({ Block, examples, schema, testConfig, validationsExamples });
+runBlockSchemaTests({ examples, schema });
