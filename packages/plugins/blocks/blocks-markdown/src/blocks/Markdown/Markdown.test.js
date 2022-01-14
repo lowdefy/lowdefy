b96394456f972386fe3395331ac0c1fb82c3ac07
Sandile <sah.memela@gmail.com>
2022-01-14 11:43:51 +0200

    fix(blocks-markdown): Refactored tests to use new Block config.

--- packages/plugins/blocks/blocks-markdown/src/blocks/Markdown/Markdown.test.js
+++ packages/plugins/blocks/blocks-markdown/src/blocks/Markdown/Markdown.test.js
@@ -16,12 +16,15 @@
 
 import { runBlockSchemaTests, runRenderTests } from '@lowdefy/block-dev';
 
-import block from './index.js';
 import Block from './Markdown.js';
 import examples from './examples.yaml';
 import schema from './schema.json';
 
-const { meta, tests } = block;
+const testConfig = {
+  validation: true,
+  required: true,
+  values: [],
+};
 
-runRenderTests({ Block, examples, meta, schema, tests });
-runBlockSchemaTests({ examples, meta, schema });
+runRenderTests({ Block, examples, schema, testConfig });
+runBlockSchemaTests({ examples, schema });
