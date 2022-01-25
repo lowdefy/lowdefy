ffc9c351590921f0008192c4106ba4fab8c82e73
João Correia <correiarmjoao@gmail.com>
2022-01-25 20:06:06 +0000

    fix(connection-knex): Fix connection-knex plugin structure to work with version 4.

--- packages/build/src/scripts/generateDefaultTypes.js
+++ packages/build/src/scripts/generateDefaultTypes.js
@@ -27,6 +27,7 @@ const defaultPackages = [
   '@lowdefy/blocks-loaders',
   '@lowdefy/blocks-markdown',
   '@lowdefy/connection-axios-http',
+  '@lowdefy/connection-knex',
   '@lowdefy/connection-redis',
   '@lowdefy/operators-change-case',
   // '@lowdefy/operators-diff',
