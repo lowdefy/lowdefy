4cbd863cfe8f2b325dc3e9bea4d54d1f4c1d97b7
Sandile <sah.memela@gmail.com>
2022-02-09 17:10:38 +0200

    fix(engine): Updated login action method test to use action & block plugin config.

---
+++ packages/engine/src/actions/login.test.js
@@ -0,0 +1,93 @@
+/*
+  Copyright 2020-2021 Lowdefy, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import testContext from '../../test/testContext.js';
+
+const lowdefy = {
+  _internal: {
+    actions: {
+      Login: ({ methods: { login }, params }) => {
+        return login(params);
+      },
+    },
+    blockComponents: {
+      Button: { meta: { category: 'display' } },
+    },
+    auth: {
+      login: jest.fn(),
+    },
+  },
+};
+
+const RealDate = Date;
+const mockDate = jest.fn(() => ({ date: 0 }));
+mockDate.now = jest.fn(() => 0);
+
+beforeEach(() => {
+  global.Date = mockDate;
+  lowdefy._internal.auth.login.mockReset();
+});
+
+afterAll(() => {
+  global.Date = RealDate;
+});
+
+test('Login', async () => {
+  const rootBlock = {
+    blockId: 'root',
+    meta: {
+      category: 'container',
+    },
+    areas: {
+      blocks: [
+        {
+          id: 'button',
+          blockId: 'button',
+          type: 'Button',
+          meta: {
+            category: 'display',
+            valueType: 'string',
+          },
+          events: {
+            onClick: [
+              {
+                id: 'a',
+                type: 'Login',
+                params: { input: { i: true }, pageId: 'pageId', urlQuery: { u: true } },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+  const context = await testContext({
+    lowdefy,
+    rootBlock,
+  });
+  const button = context._internal.RootBlocks.map['button'];
+  const res = await button.triggerEvent({ name: 'onClick' });
+  expect(lowdefy._internal.auth.login.mock.calls).toEqual([
+    [
+      {
+        input: { i: true },
+        pageId: 'pageId',
+        urlQuery: { u: true },
+      },
+    ],
+  ]);
+  expect(res.success).toBe(true);
+});
