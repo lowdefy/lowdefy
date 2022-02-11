9988b3051a0949d568d59c97d611c40e21cceb41
Sandile <sah.memela@gmail.com>
2022-02-11 15:47:39 +0200

    chore(engine): Remove comments to silence errors and logs from createRequest test.

--- packages/engine/src/actions/createRequest.test.js
+++ packages/engine/src/actions/createRequest.test.js
@@ -56,8 +56,8 @@ const mockDate = jest.fn(() => ({ date: 0 }));
 mockDate.now = jest.fn(() => 0);
 
 // Comment out to use console
-// console.log = () => {};
-// console.error = () => {};
+console.log = () => {};
+console.error = () => {};
 
 beforeAll(() => {
   global.Date = mockDate;
