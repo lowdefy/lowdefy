1398ca3506a5cd4f116b87feb7feb7f06e3de518
Gervwyk <gervwyk@gmail.com>
2022-03-03 13:59:16 +0200

    feat(build): Add loading and skeleton to blocks schema.

--- packages/build/src/lowdefySchema.js
+++ packages/build/src/lowdefySchema.js
@@ -219,10 +219,10 @@ export default {
             type: 'Block "layout" should be an object.',
           },
         },
-        loading: {
-          type: 'object',
+        skeleton: {
+          type: 'array',
           errorMessage: {
-            type: 'Block "loading" should be an object.',
+            type: 'Block "skeleton" should be an array.',
           },
         },
         style: {
@@ -232,6 +232,7 @@ export default {
           },
         },
         visible: {},
+        loading: {},
         blocks: {
           type: 'array',
           items: {
