c37129a11b6e1e34faca0c0108dde7abc132ddc9
Gervwyk <gervwyk@gmail.com>
2022-04-29 12:10:55 +0200

    chore: Fix typos.

--- packages/server-dev/lib/utils/request.js
+++ packages/server-dev/lib/utils/request.js
@@ -20,8 +20,11 @@ async function request({ url, method = 'GET', body }) {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(body),
+    body: body && JSON.stringify(body),
   });
+  if (res.status === 404) {
+    return null;
+  }
   if (!res.ok) {
     // TODO: check
     const body = await res.json();
