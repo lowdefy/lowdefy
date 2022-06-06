bc5b12fce379352e6cbf73503154e88b980918e6
Sam <sam@lowdefy.com>
2022-06-06 15:32:49 +0200

    feat(cli): Add —no-open option to cli dev command.

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -69,6 +69,7 @@ program
     'Change config directory. Default is the current working directory.'
   )
   .option('--disable-telemetry', 'Disable telemetry.')
+  .option('--no-open', 'Do not open a new tab in the default browser.')
   .option(
     '--package-manager <package-manager>',
     'The package manager to use. Options are "npm" or "yarn".'
