fbf4b14960ad176037546fd67bbe0ababf30aa3e
Sam <sam@lowdefy.com>
2022-07-14 14:39:05 +0200

    feat: Add _intl operator.

---
+++ packages/plugins/operators/operators-js/src/operators/shared/intl.js
@@ -0,0 +1,66 @@
+/*
+  Copyright 2020-2022 Lowdefy, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/*
+// TODO: docs - arguments for all formatters
+(arguments: {
+  on: date,
+  options?: object
+  locale?: string,
+}): string
+(arguments: [
+  on: date,
+  options?: object
+  locale?: string,
+]): string
+*/
+
+import { runClass } from '@lowdefy/operators';
+
+function createFormatter({ IntlClass }) {
+  const formatter = (on, options, locale) => {
+    return new IntlClass(locale, options).format(on);
+  };
+  return formatter;
+}
+
+const meta = {
+  // TODO: Is namedArgs order correct?
+  dateTimeFormat: { namedArgs: ['on', 'options', 'locale'], validTypes: ['array', 'object'] },
+  listFormat: { namedArgs: ['on', 'options', 'locale'], validTypes: ['array', 'object'] },
+  numberFormat: { namedArgs: ['on', 'options', 'locale'], validTypes: ['array', 'object'] },
+  relativeTimeFormat: { namedArgs: ['on', 'options', 'locale'], validTypes: ['array', 'object'] },
+};
+
+const functions = {
+  dateTimeFormat: createFormatter({ IntlClass: Intl.DateTimeFormat }),
+  listFormat: createFormatter({ IntlClass: Intl.ListFormat }),
+  numberFormat: createFormatter({ IntlClass: Intl.NumberFormat }),
+  relativeTimeFormat: createFormatter({ IntlClass: Intl.RelativeTimeFormat }),
+};
+
+function intl({ params, location, methodName }) {
+  return runClass({
+    functions,
+    location,
+    meta,
+    methodName,
+    operator: '_intl',
+    params,
+  });
+}
+
+export default intl;
