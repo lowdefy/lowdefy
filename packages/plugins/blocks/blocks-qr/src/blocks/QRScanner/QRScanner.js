7794deed9e63bcd281a7fa61ce101f31f3a7f1bb
StephanieJKS <47713197+StephanieJKS@users.noreply.github.com>
2022-09-06 17:29:17 +0200

    fix(blocks-qr): Change formatsToSupport enum to handle strings.

--- packages/plugins/blocks/blocks-qr/src/blocks/QRScanner/QRScanner.js
+++ packages/plugins/blocks/blocks-qr/src/blocks/QRScanner/QRScanner.js
@@ -18,6 +18,26 @@ import React from 'react';
 import { blockDefaultProps } from '@lowdefy/block-utils';
 import { Html5Qrcode } from 'html5-qrcode';
 
+const codes = {
+  QR_CODE: 0,
+  AZTEC: 1,
+  CODABAR: 2,
+  CODE_39: 3,
+  CODE_93: 4,
+  CODE_128: 5,
+  DATA_MATRIX: 6,
+  MAXICODE: 7,
+  ITF: 8,
+  EAN_13: 9,
+  EAN_8: 10,
+  PDF_417: 11,
+  RSS_14: 12,
+  RSS_EXPANDED: 13,
+  UPC_A: 14,
+  UPC_E: 15,
+  UPC_EAN_EXTENSION: 16,
+};
+
 class QRScanner extends React.Component {
   constructor(props) {
     super(props);
@@ -34,7 +54,11 @@ class QRScanner extends React.Component {
   onStart() {
     this.html5QrCode?.start(
       { facingMode: this.props.properties.facingMode || 'environment' },
-      { ...{ aspectRatio: 1 }, ...this.props.properties },
+      {
+        ...{ aspectRatio: 1 },
+        ...this.props.properties,
+        ...{ formatsToSupport: codes[this.props.properties.formatsToSupport] },
+      },
       this.onNewScanResult
     );
   }
