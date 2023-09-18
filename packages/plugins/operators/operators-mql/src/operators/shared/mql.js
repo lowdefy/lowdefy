19dc4b3533667dad456decc1db09354e9c8fdfaa
Sam Tolmay <sam@lowdefy.com>
2023-09-18 13:03:49 +0200

    chore: Update operators-mql test for new mingo version.

--- packages/plugins/operators/operators-mql/src/operators/shared/mql.js
+++ packages/plugins/operators/operators-mql/src/operators/shared/mql.js
@@ -18,25 +18,7 @@ import mingo from 'mingo';
 import { get, type } from '@lowdefy/helpers';
 import { runClass } from '@lowdefy/operators';
 
-// TODO: fix build to work with:
-// import 'mingo/init/system';
-
-import { useOperators, OperatorType } from 'mingo/core.js';
-import * as accumulatorOperators from 'mingo/operators/accumulator/index.js';
-import * as expressionOperators from 'mingo/operators/expression/index.js';
-import * as pipelineOperators from 'mingo/operators/pipeline/index.js';
-import * as queryOperators from 'mingo/operators/query/index.js';
-import * as projectionOperators from 'mingo/operators/projection/index.js';
-
-// "import * as" is returning different object structures when the connection is being
-// imported in the build or when running the tests.
-// So we check for the a default object with all the named exports, otherwise the
-// returned object has all the named exports.
-useOperators(OperatorType.ACCUMULATOR, accumulatorOperators.default || accumulatorOperators);
-useOperators(OperatorType.EXPRESSION, expressionOperators.default || expressionOperators);
-useOperators(OperatorType.PIPELINE, pipelineOperators.default || pipelineOperators);
-useOperators(OperatorType.QUERY, queryOperators.default || queryOperators);
-useOperators(OperatorType.PROJECTION, projectionOperators.default || projectionOperators);
+import 'mingo/init/system';
 
 function aggregate(data, pipeline) {
   if (data === null) {
