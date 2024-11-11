f756f705944e72ddc461e47f944de3e838b60d62
Johann Moller <johann@lowdefy.com>
2024-11-11 16:23:20 +0200

    fix: controlSwitch tests

--- packages/api/src/routes/endpoints/control/controlTry.test.js
+++ packages/api/src/routes/endpoints/control/controlTry.test.js
@@ -13,6 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
+
 import { RequestError } from '../../../context/errors.js';
 import runTest from '../test/runTest.js';
 
