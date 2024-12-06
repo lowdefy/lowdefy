a465eb3f317c407ba59631affeb5b5de7dabf4de
Machiel <machielvanderwalt27@gmail.com>
2024-12-06 12:30:02 +0200

    chore: Include link to node cve.

--- packages/servers/server-dev/manager/processes/installPlugins.mjs
+++ packages/servers/server-dev/manager/processes/installPlugins.mjs
@@ -21,6 +21,7 @@ function installPlugins({ logger, packageManagerCmd }) {
     logger.info({ print: 'spin' }, 'Installing plugins...');
     await spawnProcess({
       processOptions: {
+        // https://nodejs.org/en/blog/vulnerability/april-2024-security-releases-2#command-injection-via-args-parameter-of-child_processspawn-without-shell-option-enabled-on-windows-cve-2024-27980---high
         shell: process.platform === 'win32',
       },
       command: packageManagerCmd,
