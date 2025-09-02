25e1dd4cdca0002845a3bfe1c7514c476d302149
Salahuddin Saiet <salahuddin@lowdefy.com>
2025-09-02 12:44:12 +0200

    fix: Sort keys when hashing

--- packages/build/src/utils/makeRefHash.js
+++ packages/build/src/utils/makeRefHash.js
@@ -1,8 +1,29 @@
+import { type } from '@lowdefy/helpers';
 import crypto from 'crypto';
+
+// Sort object keys to ensure stable stringification
+function stableStringify(obj) {
+  if (type.isObject(obj)) {
+    return (
+      '{' +
+      Object.keys(obj)
+        .sort()
+        .map((k) => `"${k}":${stableStringify(obj[k])}`)
+        .join(',') +
+      '}'
+    );
+  }
+
+  if (type.isArray(obj)) {
+    return '[' + obj.map(stableStringify).join(',') + ']';
+  }
+
+  return JSON.stringify(obj);
+}
 
 export default function makeRefHash(refDef) {
   return crypto
     .createHash('sha1')
-    .update(JSON.stringify(refDef) || '')
+    .update(stableStringify(refDef) || '')
     .digest('base64');
 }
