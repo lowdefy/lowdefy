d7fadf6d69d208a4ea29abd092d8c9c956a8784b
Salahuddin Saiet <salahuddin@lowdefy.com>
2026-02-11 14:54:14 +0200

    fix(prp): Remove local plugins

--- plugins/blocks-demo/src/schemas.js
+++
@@ -1,4 +0,0 @@
-export { default as HelloWorld } from './blocks/HelloWorld/schema.js';
-export { default as ContactSelector } from './blocks/ContactSelector/schema.js';
-export { default as DemoConnection } from './connections/DemoConnection/schema.js';
-export { default as DemoRequest } from './connections/DemoConnection/DemoRequest/schema.js';
