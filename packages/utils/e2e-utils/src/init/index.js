1fa1935e554672dfe6e00551ab850c0afc8a3680
Machiel <machiel@lowdefy.com>
2026-02-13 16:43:03 +0200

    chore(e2e-utils): Update license headers to 2026. (#1982)
    Request: "fix 1 - License headers 2024 → 2026"
    Motivation: PR review found 35 files with outdated 2024 copyright year, should be 2026 per CLAUDE.md.
    
    Changes:
    - e2e-utils/src/* (22 files): updated copyright year
    - blocks-antd/*/e2e.js (12 files): updated copyright year
    - blocks-basic/List/e2e.js: updated copyright year
    
    Tags: license-header, copyright, e2e-utils, blocks-antd, blocks-basic, housekeeping
    Refs: #1982

--- packages/utils/e2e-utils/src/init/index.js
+++ packages/utils/e2e-utils/src/init/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
-  Copyright 2020-2024 Lowdefy, Inc
+  Copyright 2020-2026 Lowdefy, Inc
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
